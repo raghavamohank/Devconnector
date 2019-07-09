3ed23b520dca474f4aa66271f85d758e74d78375
Raghavamohank <raghavamohank@gmail.com>
2019-07-09 14:47:36 +0530

    jwt authentication & validation

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
     mongoURI: 
-    'mongodb+srv://raghava:raghu@1805@cluster0-xia1x.mongodb.net/test?retryWrites=true&w=majority'
+    'mongodb+srv://raghava:raghu@1805@cluster0-xia1x.mongodb.net/test?retryWrites=true&w=majority',
+    secretOrKey: 'secret'
 }
